93c0290a9d9bde51f6c71311339a18ac0d62ee9b
mreck <mathias.reck@gmail.com>
2018-04-02 11:41:43 +0200

    add map

---
+++ map.js
@@ -0,0 +1,17 @@
+function map(obj, func) {
+	const res = [];
+	if (Array.isArray(obj)) {
+		for (var i = 0; i < obj.length; i++) {
+			res.push(func(obj[i]));
+		}
+	} else {
+		for (var key in obj) {
+			if (obj.hasOwnProperty(key)) {
+				res.push(func(obj[key]));
+			}
+		}
+	}
+	return res;
+}
+
+module.exports = map;
