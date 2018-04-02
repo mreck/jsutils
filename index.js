46851444cbccfd89e883c6cc01bbc0b51fa08c25
mreck <mathias.reck@gmail.com>
2018-04-02 12:41:21 +0200

    add obs

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var utils = {};
 utils.each = require('./each');
 utils.map = require('./map');
 utils.mapobj = require('./mapobj');
+utils.obs = require('./obs');
 
 module.exports = utils;
---
+++ obs.js
@@ -0,0 +1,38 @@
+class Obs {
+	constructor(val) {
+		this._val = val;
+		this._fn = [];
+	}
+	_notify(old_val, new_val) {
+		for (var i = 0; i < this._fn.length; i++) {
+			this._fn[i](old_val, new_val);
+		}
+	}
+	get() {
+		return this._val;
+	}
+	set(new_val) {
+		if (this._val !== new_val) {
+			const old_val = this._val;
+			this._val = new_val;
+			this._notify(old_val, new_val);
+		}
+	}
+	sub(fn) {
+		this._fn.push(fn);
+	}
+	unsub(fn) {
+		const i = this._fn.indexOf(fn);
+		if (i > -1) {
+			this._fn.splice(i, 1);
+		}
+	}
+}
+
+function obs(val) {
+	return new Obs(val);
+}
+
+obs.Obs = Obs;
+
+module.exports = obs;
