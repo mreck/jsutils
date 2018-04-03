364dab8ef59092e600187bec6d74642e1e2203dc
mreck <mathias.reck@gmail.com>
2018-04-03 11:50:04 +0200

    add src directory

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var utils = {};
 
-utils.each = require('./each');
-utils.event = require('./event');
-utils.keyListener = require('./keyListener');
-utils.map = require('./map');
-utils.mapobj = require('./mapobj');
-utils.match = require('./match');
-utils.merge = require('./merge');
-utils.obs = require('./obs');
-utils.req = require('./req');
-utils.y = require('./y');
+utils.each = require('./src/each');
+utils.event = require('./src/event');
+utils.keyListener = require('./src/keyListener');
+utils.map = require('./src/map');
+utils.mapobj = require('./src/mapobj');
+utils.match = require('./src/match');
+utils.merge = require('./src/merge');
+utils.obs = require('./src/obs');
+utils.req = require('./src/req');
+utils.y = require('./src/y');
 
 module.exports = utils;
