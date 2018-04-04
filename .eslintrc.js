fb51e72a6afaa69aa6ac149d01a143f13d0a50fe
mreck <mathias.reck@gmail.com>
2018-04-04 09:04:06 +0200

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+	'env': {
+		'browser': true,
+		'es6': true,
+		'node': true,
+	},
+	'extends': 'eslint:recommended',
+	'rules': {
+		'indent': ['error', 'tab', { 'SwitchCase': 1 }],
+		'linebreak-style': ['error', 'unix'],
+		'quotes': ['error', 'single'],
+		'semi': ['error', 'always'],
+	}
 };
