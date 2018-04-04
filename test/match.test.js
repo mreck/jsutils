f8ada35bac40a03c3051420ec916e1a48a1c6a35
mreck <mathias.reck@gmail.com>
2018-04-04 10:35:50 +0200

    add match test

---
+++ test/match.test.js
@@ -0,0 +1,13 @@
+/* eslint-env mocha */
+
+const expect = require('chai').expect;
+const match = require('../src/match');
+
+describe('match', function () {
+
+	it('should match correctly', function () {
+		const obj_map = { '1': true, 'foo': 'bar' };
+		expect(match('1', obj_map)).to.equal(true);
+		expect(match('foo', obj_map)).to.equal('bar');
+	});
+});
