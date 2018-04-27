3581313c71528917221788fc7b90a91dce40d168
mreck <mathias.reck@gmail.com>
2018-04-27 23:57:32 +0200

    fix validate.test.js typos

--- test/validator.test.js
+++ test/validator.test.js
@@ -7,14 +7,14 @@ describe('validator', function () {
 
 	describe('num', function () {
 
-		it('should validate max value correctly', function () {
+		it('should validate max values correctly', function () {
 			const v = validator.num({ max: 10 });
 			expect(v(5)).to.be.true;
 			expect(v(10)).to.be.true;
 			expect(v(11)).to.be.false;
 		});
 
-		it('should validate min value correctly', function () {
+		it('should validate min values correctly', function () {
 			const v = validator.num({ min: 5 });
 			expect(v(4)).to.be.false;
 			expect(v(5)).to.be.true;
