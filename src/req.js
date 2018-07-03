2bb794131bf187aaf93ba6e7d66892678a7b67da
mreck <mathias.reck@gmail.com>
2018-07-04 00:09:08 +0200

    Change req() to new promise pattern req is now callback based; the promise version is exposed via req.p()

--- src/req.js
+++ src/req.js
@@ -1,44 +1,56 @@
-function req(type, url, data) {
-	return new Promise(function (resolve, reject) {
-		var request = new XMLHttpRequest();
+function req(method, url, data, cb) {
+	const request = new XMLHttpRequest();
 
-		request.open(type, url, true);
+	request.open(method, url, true);
 
-		if (data) {
-			request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-		}
+	if (data) {
+		request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	}
 
-		request.onload = function () {
-			if (request.status >= 200 && request.status < 400) {
-				var d = JSON.parse(request.responseText);
-				resolve(d);
-			} else {
-				reject();
-			}
-		};
-
-		request.onerror = function () {
-			reject();
-		};
-
-		if (data) {
-			request.send(JSON.stringify(data));
+	request.onload = () => {
+		if (request.status >= 200 && request.status < 400) {
+			const d = JSON.parse(request.responseText);
+			cb(null, d);
 		} else {
-			request.send();
+			cb(new Error('status: ' + request.status), null);
 		}
-	});
+	};
+
+	request.onerror = (ev) => cb(ev, null);
+
+	if (data) {
+		request.send(JSON.stringify(data));
+	} else {
+		request.send();
+	}
 }
 
-const GET = 'GET';
-const POST = 'POST';
+req.p = (method, url, data) => new Promise((res, rej) =>
+	req(method, url, data, (err, data) => err ? rej(err) : res(data)));
 
-module.exports = {
-	json: {
-		get: function (url) {
-			return req(GET, url);
-		},
-		post: function (url, data) {
-			return req(POST, url, data);
-		}
-	}
-};
+// http methods
+req.MGET = 'GET';
+req.MPOST = 'POST';
+req.MPUT = 'PUT';
+req.MDELETE = 'DELETE';
+
+// shorthands
+req.get = (url, data, cb) => req(req.MGET, url, data, cb);
+req.post = (url, data, cb) => req(req.MPOST, url, data, cb);
+req.put = (url, data, cb) => req(req.MPUT, url, data, cb);
+req.del = (url, data, cb) => req(req.MDELETE, url, data, cb);
+
+// promise shorthands
+req.get.p = (url, data) => new Promise((res, rej) =>
+	req.get(url, data, (err, data) => err ? rej(err) : res(data)));
+
+req.post.p = (url, data) => new Promise((res, rej) =>
+	req.post(url, data, (err, data) => err ? rej(err) : res(data)));
+
+req.put.p = (url, data) => new Promise((res, rej) =>
+	req.put(url, data, (err, data) => err ? rej(err) : res(data)));
+
+req.del.p = (url, data) => new Promise((res, rej) =>
+	req.del(url, data, (err, data) => err ? rej(err) : res(data)));
+
+module.exports = req;
