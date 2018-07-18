111de4c5d9b2dbbf453a3d3a787ceeeb891499f7
mreck <mathias.reck@gmail.com>
2018-07-18 14:22:54 +0200

    Extract key codes into their own file

--- src/keyListener.js
+++ src/keyListener.js
@@ -1,24 +1,5 @@
 const event = require('./event');
-
-const KEY_CODE = {
-	BACKSPACE: 8,
-	TAB: 9,
-	ENTER: 13,
-	SHIFT: 16,
-	CTRL: 17,
-	ALT: 19,
-	ESCAPE: 27,
-	PAGE_UP: 33,
-	PAGE_DOWN: 34,
-	END: 35,
-	HOME: 36,
-	ARROW_LEFT: 37,
-	ARROW_UP: 38,
-	ARROW_RIGHT: 39,
-	ARROW_DOWN: 40,
-	INSERT: 45,
-	DELETE: 46,
-};
+const keyCode = require('./keyCode');
 
 class KeyListener {
 	constructor(node) {
@@ -36,11 +17,11 @@ class KeyListener {
 	}
 }
 
-function keyListener(node) {
+function create(node) {
 	return new KeyListener(node || document);
 }
 
-keyListener.CODE = KEY_CODE;
-keyListener.KeyListener = KeyListener;
+create.CODE = keyCode;
+create.KeyListener = KeyListener;
 
-module.exports = keyListener;
+module.exports = create;
