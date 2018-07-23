3ac7ba2e341daefdc6f3562a77a0ce3918f723b1
mreck <mathias.reck@gmail.com>
2018-07-23 22:57:41 +0200

    add mkdir

--- node.js
+++ node.js
@@ -4,5 +4,6 @@ utils.fsfiletree = require('./src/fsfiletree');
 utils.fsrdir = require('./src/fsrdir');
 utils.fsrjson = require('./src/fsrjson');
 utils.fswjson = require('./src/fswjson');
+utils.mkdir = require('./src/mkdir');
 
 module.exports = utils;
