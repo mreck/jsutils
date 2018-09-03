118faaa6863bea4fa4ba05e9be928b23cbf94033
mreck <mathias.reck@gmail.com>
2018-09-03 23:12:35 +0200

    add merge.safe()

--- test/merge.test.js
+++ test/merge.test.js
@@ -19,4 +19,22 @@ describe('merge', function () {
 		const t = merge({}, d1, d2, d3);
 		expect(t).to.deep.equal({ a: 6, b: 2, c: 3, d: 5, e: 7 });
 	});
+
+	describe('merge.safe', function () {
+
+		it('should handle one piece of data', function () {
+			const t = { a: 1, b: 2, c: 3 };
+			const d = { a: 4, d: 5 };
+			merge.safe(t, d);
+			expect(t).to.deep.equal({ a: 1, b: 2, c: 3, d: 5 });
+		});
+
+		it('should handle multiple pieces of data', function () {
+			const d1 = { a: 1, b: 2, c: 3 };
+			const d2 = { a: 4, d: 5 };
+			const d3 = { a: 6, e: 7 };
+			const t = merge.safe({}, d1, d2, d3);
+			expect(t).to.deep.equal({ a: 1, b: 2, c: 3, d: 5, e: 7 });
+		});
+	});
 });
