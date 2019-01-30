c0fe82cbbf1379b949d0a9ef34b5b79a997da15d
mreck <mreck@users.noreply.github.com>
2019-01-30 16:03:08 +0100

    Remove node js utils
    Migrated to @mreck/nodejsutils

--- nodejs.js
+++
@@ -1,9 +0,0 @@
-const utils = require('./lib');
-
-utils.fsfiletree = require('./lib/fsfiletree');
-utils.fsrdir = require('./lib/fsrdir');
-utils.fsrjson = require('./lib/fsrjson');
-utils.fswjson = require('./lib/fswjson');
-utils.mkdir = require('./lib/mkdir');
-
-module.exports = utils;
