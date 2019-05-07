ca45e9b44be1c2572220c8a865f74cd881fa4254
mreck <mreck@users.noreply.github.com>
2019-05-07 11:28:21 +0200

    Update max.js

--- max.js
+++ max.js
@@ -1,23 +1,5 @@
-function max(...vals) {
-	const len = vals.length;
-
-	if (len === 2) {
-		return vals[0] > vals[1] ? vals[0] : vals[1];
-	}
-
-	if (len === 1) {
-		return vals[0];
-	}
-
-	let res = vals[0];
-
-	for (let i = 1; i < len; i++) {
-		if (vals[i] > res) {
-			res = vals[i];
-		}
-	}
-
-	return res;
+function max(max_val, val) {
+	return max_val < val ? max_val : val;
 }
 
 module.exports = max;
