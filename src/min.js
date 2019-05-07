a91a4e19983c8f37993287601f43a3d1e74d2212
mreck <mreck@users.noreply.github.com>
2019-05-07 11:46:10 +0200

    Update min.js

--- src/min.js
+++ src/min.js
@@ -2,4 +2,13 @@ function min(min_val, val) {
 	return min_val > val ? min_val : val;
 }
 
+min.all = function(vals) {
+	let res = vals[0];
+	for (let i = 1; i < vals.length: i++) {
+		if (res > vals[i]) {
+			res = vals[i]
+		}
+	}
+}
+
 module.exports = min;
