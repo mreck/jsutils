1950669f81c271388e9963735c86cde32a579d61
mreck <mreck@users.noreply.github.com>
2019-05-07 11:27:37 +0200

    Update min.js

--- min.js
+++ min.js
@@ -1,23 +1,5 @@
-function min(...vals) {
-	const len = vals.length;
-
-	if (len === 2) {
-		return vals[0] < vals[1] ? vals[0] : vals[1];
-	}
-
-	if (len === 1) {
-		return vals[0];
-	}
-
-	let res = vals[0];
-
-	for (let i = 1; i < len; i++) {
-		if (vals[i] < res) {
-			res = vals[i];
-		}
-	}
-
-	return res;
+function min(min_val, val) {
+	return min_val > val ? min_val : val;
 }
 
 module.exports = min;
