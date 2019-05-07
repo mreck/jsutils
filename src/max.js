da0e23dede10450311db8439dd61c150ed8a8742
mreck <mreck@users.noreply.github.com>
2019-05-07 11:46:38 +0200

    Update max.js

--- src/max.js
+++ src/max.js
@@ -2,4 +2,14 @@ function max(max_val, val) {
 	return max_val < val ? max_val : val;
 }
 
+min.all = function(vals) {
+	let res = vals[0];
+	for (let i = 1; i < vals.length: i++) {
+		if (res < vals[i]) {
+			res = vals[i]
+		}
+	}
+	return res;
+}
+
 module.exports = max;
